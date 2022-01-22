095869441fdfe67c71a22573983e840e8a42fb29
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-22 14:37:05 +0700

    basic register route

--- packages/backend/src/routes/register/index.ts
+++ packages/backend/src/routes/register/index.ts
@@ -1,9 +1,29 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
+interface RegisterBody {
+  username: string
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  telephoneNumber: string
+}
+
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
-  instance.get('/', async () => {
+  instance.post<{ Body: RegisterBody }>('/', async (request) => {
+    console.log(request.body)
+
+    try {
+      await instance.pg.query(
+        'insert into register (customer_name, customer_surname, phone, email, customer_password) values ($1, $2, $3, $4, $5)',
+        [request.body.firstName, request.body.lastName, request.body.telephoneNumber, request.body.email, request.body.password]
+      )
+    } catch (error) {
+      throw new Error(error as string)
+    }
+
     return {
-      message: 'users route'
+      message: 'register route'
     }
   })
 }
--- packages/frontend/src/views/register.vue
+++ packages/frontend/src/views/register.vue
@@ -3,35 +3,68 @@
     <div class="grid grid-cols-2 gap-3 text-center text-1xl grow mt-10">
       <div class="col-span-2 mb-2 text-4xl">สมัครสมาชิก</div>
       <p>Username :</p>
-      <input type="Username" placeholder="Username" class="rounded ring-black ring-2 w-60" />
+      <input type="Username" placeholder="Username" class="rounded ring-black ring-2 w-60" v-model="username">
       <p>Password :</p>
-      <input type="password" placeholder="Password" class="rounded ring-black ring-2 w-60" />
+      <input type="password" placeholder="Password" class="rounded ring-black ring-2 w-60" v-model="password">
       <p>ชื่อ :</p>
-      <input type="name" placeholder="ชื่อ" class="rounded ring-black ring-2 w-60" />
+      <input type="name" placeholder="ชื่อ" class="rounded ring-black ring-2 w-60" v-model="firstName">
       <p>นามสกุล :</p>
-      <input type="lastname" placeholder="นามสกุล" class="rounded ring-black ring-2 w-60" />
+      <input type="lastname" placeholder="นามสกุล" class="rounded ring-black ring-2 w-60" v-model="lastName">
       <p>Email :</p>
-      <input type="email" placeholder="Email" class="rounded ring-black ring-2 w-60" />
+      <input type="email" placeholder="Email" class="rounded ring-black ring-2 w-60" v-model="email">
       <p>เบอร์โทร :</p>
-      <input type="tel" placeholder="เบอร์โทร" class="rounded ring-black ring-2 w-60" />
-      <button class="hover:bg-orange-600 col-span-2 mx-auto h-min w-max px-2 bg-main rounded font-bold text-2xl mt-4" @click="register.get()">สมัครสมาชิก</button>
+      <input type="tel" placeholder="เบอร์โทร" class="rounded ring-black ring-2 w-60" v-model="telephoneNumber">
+      <button class="hover:bg-orange-600 col-span-2 mx-auto h-min w-max px-2 bg-main rounded font-bold text-2xl mt-4" @click="registerUser">สมัครสมาชิก</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, ref } from 'vue'
 import ky from 'ky'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'register',
-  setup(){
-    onMounted(async()=> {
+  setup () {
+    const router = useRouter()
+
+    const username = ref('')
+    const password = ref('')
+    const firstName = ref('')
+    const lastName = ref('')
+    const email = ref('')
+    const telephoneNumber = ref('')
+
+    const registerUser = async (): Promise<void> => {
       try {
-        const response = await ky.get('http://localhost:4000/register').json()
+        const response = await ky.post('http://localhost:4000/register', {
+          json: {
+            username: username.value,
+            password: password.value,
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            telephoneNumber: telephoneNumber.value
+          }
+        }).json()
+
         console.log(response)
-      } catch(error){}
-    })
+        router.push('/')
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    return {
+      username,
+      password,
+      firstName,
+      lastName,
+      email,
+      telephoneNumber,
+      registerUser
+    }
   }
 })
 </script>
